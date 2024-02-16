5aeccc5795db7e70890bc35968443ef186d64c60
Tomas Pardo <t.pardo@uniandes.edu.co>
2024-02-15 22:32:36 -0500

    pipi

--- p0/parser1.py
+++ p0/parser1.py
@@ -10,7 +10,7 @@ class Stack:
     def push(self, token):
         self.tokens.append(token)
 
-    def pop(self):
+    def pope(self):
         if not self.is_empty():
             return self.tokens.pop()
         else:
@@ -24,16 +24,56 @@ class Stack:
 
     def size(self):
         return len(self.tokens)
+    
+def array_to_stack(arr):
+    stack = Stack()
+    for item in reversed(arr):
+        stack.push(item)
+    return stack
 
 class Parser:
-    def __init__(tokens):
-        self.tokens = tokens
+    def __init__(self, tokens):
+        self.tokens = array_to_stack(tokens)
         self.tok_idx = 1
         self.advance()
+        self.defun()
 
-    def advance( ):
-        self.tok_idx +=1
-        if self.tok_idx < len(self.tokens):
-            self.current_tok = self.tokens[self.tok_idx]
+    def advance(self):
 
-  
+        self.current_tok = self.tokens.pope()
+        return self.current_tok
+    
+    def defun(self): 
+        if self.current_tok != 'varName':
+            print('no')
+        self.advance()
+        if self.current_tok  != 'number':
+            print('no')
+        self.advance()
+        if self.current_tok  != 'right':
+            print('no')
+
+    def move(self):
+        if self.current_tok != 'number':
+            print ('no')
+        self.advance()
+        if self.current_tok  != 'right':
+            print('no')
+
+    def skip(self):
+        if self.current_tok != 'numer':
+            print('no')
+        if self.current_tok  != 'right':
+            print('no')
+    
+    def turn(self):
+        odds = ['tLeft', 'tRight', 'tAround']
+
+    
+def run (text):
+    lexer = lex.iniciar(text)
+    
+    parser = Parser(lexer)
+
+run("peludo 3)")
+    
